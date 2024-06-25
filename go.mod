765b4887c5a86acbfc18109d027bb16c2f835cb4
RayZarei <ahmadreza0zarei@gmail.com>
2024-06-25 09:54:35 +0330

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AhmadRezaZarei/my-logger2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,13 @@
+package mylogger
+
+func LogInfo(message string) {
+
+}
+
+func LogWarning(message string) {
+
+}
+
+func LogError(message string) {
+
+}
